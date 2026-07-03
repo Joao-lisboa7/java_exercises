33c691c9ddabd3bdfcf98c8ffa84b758ca664ef0
João Pedro <jplisboa.dev@gmail.com>
2026-07-03 16:06:21 -0300

    feat: add features

--- modulo12-order_management_system/entities/Order.java
+++ modulo12-order_management_system/entities/Order.java
@@ -1,10 +1,14 @@
 package entities;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import enums.OrderStatus;
 
 public class Order {
   private Date moment;
   private OrderStatus status;
+
+  private List<OrderItem> items = new ArrayList<>();
 
   public Order(){}
 
@@ -30,7 +34,25 @@ public class Order {
   }
 
   public void addItem(OrderItem item){
-  /*Implementar*/
+    items.add(item);
   }
-  
+
+  public void removeItem(OrderItem item){
+    items.remove(item);
+  }
+
+  public Double total(){
+    Double sum = 0.0;
+    for(int i = 0; i < items.size(); i++){
+      sum += items.get(i).subTotal();
+    }
+
+    return sum;
+  }
+
+  @Override
+  public String toString() {
+    return "Order [moment=" + moment + ", status=" + status + ", items=" + items + "]";
+  }
+
 }
